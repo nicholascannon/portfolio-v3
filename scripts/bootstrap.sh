d9d04963dc3cdd6519943f8d75f90d8dd6aaeb65
Nicholas Cannon <nicholascannon1@gmail.com>
2021-03-13 19:06:57 +0800

    created pull request workflow

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "setting up infra..."
+cd infra && npm install --silent
+echo "infra installed!"
+
+cd ..
+
+echo "setting up frontend..."
+cd frontend && npm install --silent
+echo "frontend installed!"
