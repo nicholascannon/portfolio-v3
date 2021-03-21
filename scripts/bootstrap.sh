706748582ec8175662ed34f38b083ba3bca933bc
Nicholas Cannon <nicholascannon1@gmail.com>
2021-03-21 17:52:52 +0800

    Get blob function (#6)
    * added getBlob function
    
    * added functions to install script
    
    * added lambda role + policies

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,3 +9,9 @@ cd ..
 echo "setting up frontend..."
 cd frontend && npm install --silent
 echo "frontend installed!"
+
+cd ..
+
+echo "setting up functions..."
+cd functions/getBlob && npm install --silent
+echo "functions installed!"
