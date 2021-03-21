1ff416f6737f67298fc2802c22da78d19595d9d6
Nicholas Cannon <nicholascannon1@gmail.com>
2021-03-21 16:54:09 +0800

    added getBlob function

---
+++ functions/getBlob/index.js
@@ -0,0 +1,38 @@
+const {DynamoDB} = require('@aws-sdk/client-dynamodb');
+const {unmarshall} = require('@aws-sdk/util-dynamodb');
+
+const db = new DynamoDB({region: 'ap-southeast-2'});
+
+const Response = (status, body, headers) => ({
+	statusCode: status,
+	body: JSON.stringify(body),
+	headers: {
+		'Content-Type': 'application/json',
+		'Access-Control-Allow-Origin': '*',
+		...headers
+	}
+});
+
+const getBlob = async (event, context) => {
+	try {
+		const projects = await db.scan({TableName: 'portfolio-project-table-40b7807'});
+		const about = await db.scan({TableName: 'portfolio-about-table-cd2f94b'});
+
+		const projectsStatus = projects.$metadata.httpStatusCode;
+		const aboutStatus = about.$metadata.httpStatusCode;
+		if (projectsStatus !== 200)
+			throw new Error(`Error fetching projects, status code = ${projectsStatus}`);
+		if (projectsStatus !== 200)
+			throw new Error(`Error fetching about, status code = ${aboutStatus}`);
+
+		return Response(200, {
+			about: about.Items.length ? unmarshall(about.Items[0]) : null,
+			projects: projects.Items.map(project => unmarshall(project))
+		});
+	} catch (e) {
+		console.error(e.message);
+		return Response(400, {message: e.message});
+	}
+};
+
+module.exports.getBlob = getBlob;
