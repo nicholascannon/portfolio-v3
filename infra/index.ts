eaff1f93401309cb8863814168f1c40c7b88122b
Nicholas Cannon <nicholascannon1@gmail.com>
2021-03-27 16:13:21 +0800

    created pulumi stack to provision acm cert

--- infra/index.ts
+++ infra/index.ts
@@ -224,51 +224,6 @@ const apiRec = new aws.route53.Record(`portfolio-record-api-a-${stack}`, {
 });
 
 //-------------------------------------------------------------------------------
-// CERT SETUP
-//-------------------------------------------------------------------------------
-const cert = new aws.acm.Certificate(`portfolio-cert-${stack}`, {
-  domainName: "niccannon.com",
-  subjectAlternativeNames: ["www.niccannon.com"],
-  validationMethod: "DNS",
-  tags: {
-    project: `portfolio-${stack}`,
-  },
-});
-
-const validationOpts = cert.domainValidationOptions.apply((dvo) =>
-  dvo.map((rec) => ({
-    name: rec.resourceRecordName,
-    value: rec.resourceRecordValue,
-    type: rec.resourceRecordType,
-    domainName: rec.domainName,
-  }))
-);
-
-const valRecords = validationOpts.apply((opts) =>
-  opts.map(
-    (opt) =>
-      new aws.route53.Record(`portfolio-val-rec-${opt.domainName}-${stack}`, {
-        allowOverwrite: true,
-        name: opt.name,
-        records: [opt.value],
-        ttl: 60,
-        type: opt.type,
-        zoneId,
-      })
-  )
-);
-
-const certValidation = new aws.acm.CertificateValidation(
-  `portfolio-cert-val-${stack}`,
-  {
-    certificateArn: cert.arn,
-    validationRecordFqdns: valRecords.apply((recs) =>
-      recs.map((record) => record.fqdn)
-    ),
-  }
-);
-
-//-------------------------------------------------------------------------------
 // CDN SETUP
 //-------------------------------------------------------------------------------
 const distribution = new aws.cloudfront.Distribution(
@@ -347,5 +302,4 @@ const distribution = new aws.cloudfront.Distribution(
 
 export const bucketUrl = feBucket.websiteEndpoint;
 export const apiUrl = api.url;
-export const certArn = certValidation.certificateArn;
 export const distributionDomain = distribution.domainName;
---
+++ portfolio-cert/index.ts
@@ -0,0 +1,51 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+
+const config = new pulumi.Config();
+const stack = pulumi.getStack();
+
+const zoneId = config.require("hostedZoneId");
+
+const cert = new aws.acm.Certificate(`portfolio-cert-${stack}`, {
+  domainName: "niccannon.com",
+  subjectAlternativeNames: ["*.niccannon.com", "www.niccannon.com"],
+  validationMethod: "DNS",
+  tags: {
+    project: `portfolio-${stack}`,
+  },
+});
+
+const validationOpts = cert.domainValidationOptions.apply((dvo) =>
+  dvo.map((rec) => ({
+    name: rec.resourceRecordName,
+    value: rec.resourceRecordValue,
+    type: rec.resourceRecordType,
+    domainName: rec.domainName,
+  }))
+);
+
+const valRecords = validationOpts.apply((opts) =>
+  opts.map(
+    (opt) =>
+      new aws.route53.Record(`portfolio-val-rec-${opt.domainName}-${stack}`, {
+        allowOverwrite: true,
+        name: opt.name,
+        records: [opt.value],
+        ttl: 60,
+        type: opt.type,
+        zoneId,
+      })
+  )
+);
+
+const certValidation = new aws.acm.CertificateValidation(
+  `portfolio-cert-val-${stack}`,
+  {
+    certificateArn: cert.arn,
+    validationRecordFqdns: valRecords.apply((recs) =>
+      recs.map((record) => record.fqdn)
+    ),
+  }
+);
+
+export const certArn = certValidation.certificateArn;
