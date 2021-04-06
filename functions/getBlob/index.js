32cad58972e951391884d69692cf21daaab4d495
Nicholas Cannon <nicholascannon1@gmail.com>
2021-04-06 21:08:57 +0800

    code cleanup (#26)

--- functions/getBlob/index.js
+++ functions/getBlob/index.js
@@ -1,38 +1,44 @@
-const {DynamoDB} = require('@aws-sdk/client-dynamodb');
-const {unmarshall} = require('@aws-sdk/util-dynamodb');
+const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+const { unmarshall } = require("@aws-sdk/util-dynamodb");
 
-const db = new DynamoDB({region: 'ap-southeast-2'});
+const db = new DynamoDB({ region: "ap-southeast-2" });
 
 const Response = (status, body, headers) => ({
-	statusCode: status,
-	body: JSON.stringify(body),
-	headers: {
-		'Content-Type': 'application/json',
-		'Access-Control-Allow-Origin': '*',
-		...headers
-	}
+  statusCode: status,
+  body: JSON.stringify(body),
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    ...headers,
+  },
 });
 
 const getBlob = async (event, context) => {
-	try {
-		const projects = await db.scan({TableName: `${process.env.TABLE_PREFIX}-projects`});
-		const about = await db.scan({TableName: `${process.env.TABLE_PREFIX}-about`});
+  try {
+    const projects = await db.scan({
+      TableName: `${process.env.TABLE_PREFIX}-projects`,
+    });
+    const about = await db.scan({
+      TableName: `${process.env.TABLE_PREFIX}-about`,
+    });
 
-		const projectsStatus = projects.$metadata.httpStatusCode;
-		const aboutStatus = about.$metadata.httpStatusCode;
-		if (projectsStatus !== 200)
-			throw new Error(`Error fetching projects, status code = ${projectsStatus}`);
-		if (projectsStatus !== 200)
-			throw new Error(`Error fetching about, status code = ${aboutStatus}`);
+    const projectsStatus = projects.$metadata.httpStatusCode;
+    const aboutStatus = about.$metadata.httpStatusCode;
+    if (projectsStatus !== 200)
+      throw new Error(
+        `Error fetching projects, status code = ${projectsStatus}`
+      );
+    if (projectsStatus !== 200)
+      throw new Error(`Error fetching about, status code = ${aboutStatus}`);
 
-		return Response(200, {
-			about: about.Items.length ? unmarshall(about.Items[0]) : null,
-			projects: projects.Items.map(project => unmarshall(project))
-		});
-	} catch (e) {
-		console.error(e.message);
-		return Response(400, {message: e.message});
-	}
+    return Response(200, {
+      about: about.Items.length ? unmarshall(about.Items[0]) : null,
+      projects: projects.Items.map((project) => unmarshall(project)),
+    });
+  } catch (e) {
+    console.error(e.message);
+    return Response(400, { message: e.message });
+  }
 };
 
 module.exports.getBlob = getBlob;
