22e75760bf334ef98bcec6feb4fce0f730431aa9
Nicholas Cannon <nicholascannon1@gmail.com>
2021-05-22 13:34:45 +0800

    squash

--- infra/index.ts
+++ infra/index.ts
@@ -1,6 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 import * as fs from "fs";
 import * as path from "path";
 import * as mime from "mime";
@@ -187,15 +186,27 @@ const getBlobFunc = new aws.lambda.Function(`portfolio-f-get-blob-${stack}`, {
 });
 
 // API
-const api = new awsx.apigateway.API(`portfolio-api-${stack}`, {
-  routes: [
-    {
-      path: "/blob",
-      method: "GET",
-      eventHandler: getBlobFunc,
-    },
-  ],
-  stageName: stack,
+// const api = new awsx.apigateway.API(`portfolio-api-${stack}`, {
+//   routes: [
+//     {
+//       path: "/blob",
+//       method: "GET",
+//       eventHandler: getBlobFunc,
+//     },
+//   ],
+//   stageName: stack,
+// });
+
+const api = new aws.apigatewayv2.Api(`portfolio-api-${stack}`, {
+  protocolType: "HTTP",
+});
+
+const getBlobIntegration = new aws.apigatewayv2.Integration(`portfolio-get-blob-integration-${stack}`, {
+  apiId: api.id,
+  connectionType: "INTERNET",
+  integrationType: "AWS_PROXY",
+  integrationMethod: "GET",
+  integrationUri: getBlobFunc.invokeArn,
 });
 
 // CDN
@@ -213,9 +224,7 @@ const distribution = new aws.cloudfront.Distribution(
       },
       {
         // the url contains the stage path (the stack)
-        domainName: api.url.apply((url) =>
-          url.replace("https://", "").replace(`/${stack}/`, "")
-        ),
+        domainName: api.apiEndpoint,
         customOriginConfig: {
           httpPort: 80,
           httpsPort: 443,
@@ -311,6 +320,6 @@ const homeRecCNAME = new aws.route53.Record(
 );
 
 export const bucketUrl = feBucket.websiteEndpoint;
-export const apiUrl = api.url;
+export const apiUrl = api.apiEndpoint;
 export const distributionDomain = distribution.domainName;
 export const oai = originAccessIdentity.iamArn;
