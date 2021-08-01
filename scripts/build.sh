46f476df0b8c2295513fe46da1c2a9b09d27815c
Nicholas Cannon <nicholascannon1@gmail.com>
2021-08-01 19:17:35 +0800

    updated project scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,3 @@ set -e
 echo "building frontend..."
 cd frontend && npm run build
 echo "frontend built!"
-
-cd ..
-
-echo "building getBlob lambda..."
-cd functions/getBlob
-npm run build
-echo "getBlob lambda built!"
