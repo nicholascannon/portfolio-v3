9b51984551a827d7e230b266fd1d692370b3e137
Nicholas Cannon <nicholascannon1@gmail.com>
2021-09-27 19:04:12 +0800

    reformat lambda code

--- backend/src/getBlob.ts
+++ backend/src/getBlob.ts
@@ -1,8 +1,4 @@
-import {
-  APIGatewayProxyEvent,
-  Handler,
-  APIGatewayProxyResult,
-} from "aws-lambda";
+import { APIGatewayProxyEvent, Handler, APIGatewayProxyResult } from "aws-lambda";
 import DynamoDB from "aws-sdk/clients/dynamodb";
 
 const db = process.env.IS_OFFLINE
@@ -20,41 +16,36 @@ const Response = (status: number, body: any, headers?: any) => ({
   },
 });
 
-export const handler: Handler<APIGatewayProxyEvent, APIGatewayProxyResult> =
-  async (event, context) => {
-    try {
-      const projects = await db
-        .scan({
-          TableName: `${process.env.TABLE_PREFIX}-projects`,
-        })
-        .promise();
-      const about = await db
-        .scan({
-          TableName: `${process.env.TABLE_PREFIX}-about`,
-        })
-        .promise();
+export const handler: Handler<APIGatewayProxyEvent, APIGatewayProxyResult> = async (event, context) => {
+  try {
+    const projects = await db
+      .scan({
+        TableName: `${process.env.TABLE_PREFIX}-projects`,
+      })
+      .promise();
+    const about = await db
+      .scan({
+        TableName: `${process.env.TABLE_PREFIX}-about`,
+      })
+      .promise();
 
-      const projectsStatus = projects.$response.httpResponse.statusCode;
-      const aboutStatus = about.$response.httpResponse.statusCode;
-      if (projectsStatus !== 200) {
-        console.error(
-          `Error fetching projects: status=${projectsStatus}, error=${projects.$response.error}`
-        );
-        return Response(500, { message: "Could not fetch list of projects" });
-      }
-      if (aboutStatus !== 200) {
-        console.error(
-          `Error fetching about: status=${aboutStatus}, error=${about.$response.error}`
-        );
-        return Response(500, { message: "Could not fetch about information" });
-      }
-
-      return Response(200, {
-        about: about.Items?.length ? unmarshall(about.Items[0]) : undefined,
-        projects: projects.Items?.map((project) => unmarshall(project)),
-      });
-    } catch (e) {
-      console.error(e.message);
-      return Response(500, { message: "Oops, an error has occurred!" });
+    const projectsStatus = projects.$response.httpResponse.statusCode;
+    const aboutStatus = about.$response.httpResponse.statusCode;
+    if (projectsStatus !== 200) {
+      console.error(`Error fetching projects: status=${projectsStatus}, error=${projects.$response.error}`);
+      return Response(500, { message: "Could not fetch list of projects" });
     }
-  };
+    if (aboutStatus !== 200) {
+      console.error(`Error fetching about: status=${aboutStatus}, error=${about.$response.error}`);
+      return Response(500, { message: "Could not fetch about information" });
+    }
+
+    return Response(200, {
+      about: about.Items?.length ? unmarshall(about.Items[0]) : undefined,
+      projects: projects.Items?.map((project) => unmarshall(project)),
+    });
+  } catch (e) {
+    console.error(e.message);
+    return Response(500, { message: "Oops, an error has occurred!" });
+  }
+};
